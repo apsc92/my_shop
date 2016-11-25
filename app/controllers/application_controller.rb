c830de1361421f1c527bb7656453435ffdba9370
apsc92 <anurag@joshsoftware.com>
2016-11-26 01:21:20 +0530

    Added product listing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  private
+
+  def current_order
+    @current_order ||= begin
+      if has_order?
+        @current_order
+      else
+        order = Order.new(status: 'building')
+        session[:order_id] = order.id
+        order
+      end
+    end
+  end
+
+  def has_order?
+    session[:order_id] && @current_order = Order.includes(:order_items).find(session[:order_id])
+  end
+
+  helper_method :current_order, :has_order?
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,14 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
   has_one :address, dependent: :destroy
+
+  private
+
+  def total_items
+    order_items.sum(:quantity)
+  end
+
+  def total_cost
+    order_items.sum(:cost)
+  end
 end
