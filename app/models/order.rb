85d25a5a55ab1be7fbb078526582a47aa0d0833e
apsc92 <anurag@joshsoftware.com>
2016-11-26 23:12:45 +0530

    Considering bundle discount while calculating total cost

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,16 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
   has_one :address, dependent: :destroy
-  has_many :promocodes
+  accepts_nested_attributes_for :address
+
+  before_validation do
+    if self.status == 'building' && self.customer.nil?
+      # Dummy Customer
+      customer = Customer.create(name: 'customer1', email: 'customer1@gmail.com')
+      self.customer = customer
+      self.save
+    end
+  end
 
   def add_item(product, quantity)
     existing_order_item = self.order_items.where(product: product).first
@@ -25,6 +34,18 @@ class Order < ApplicationRecord
   end
 
   def total_cost
-    order_items.sum(&:cost)
+    cost = order_items.sum(&:cost)
+    # apply procomode if any
+    applied_promos = Promocode.where(id: self.applied_promo_ids)
+    if applied_promos.exists?
+      applied_promos.each do |promo|
+        if promo.promo_type == 'percentage'
+          cost -= (cost * promo.percentage) / 100
+        else
+          cost -= promo.flat_discount
+        end
+      end
+    end
+    cost.round(2)
   end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,22 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
-  has_one :product, as: :ordered_item
+  belongs_to :product
   validates :quantity, presence: :true, numericality: { greater_than: 0 }
+
+
+  def cost
+    bundle_discount = BundleDiscount.where(targeted_product_id: self.product.id).first
+    if bundle_discount
+      bundles = quantity / bundle_discount.quantity_to_be_purchased
+      cost = 0
+      if bundles > 0
+        cost = bundle_discount.price_after_discount
+        remainder = self.quantity % bundle_discount.quantity_to_be_purchased
+        cost += remainder * self.product.price
+      else
+        cost = self.product.price * self.quantity
+      end
+    end
+    cost
+  end
 end
